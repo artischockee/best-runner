7f2650c597a6d6db138895a16a97033020ab0a62
Artem Piskarev <artyeug@gmail.com>
2020-08-20 01:30:43 +0700

    Implement LayoutBuilder component (template)

---
+++ src/components/layoutBuilder/LayoutBuilder.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense } from "react";
+import { Router } from "../../services/router/types";
+import PageWrapper from "../pageWrapper";
+
+interface Props extends Router.RouteComponentProps {
+  component: Router.RouteComponent;
+  componentSettings: Router.ComponentSettings;
+}
+
+export default function LayoutBuilder(props: Props) {
+  const { component: Component, componentSettings } = props;
+
+  function renderBasingOnSettings() {
+    const component = <Component subRoutes={props.subRoutes} />;
+
+    if (componentSettings.usePageWrapper) {
+      return <PageWrapper withHeader={componentSettings.useHeader}>{component}</PageWrapper>;
+    }
+  }
+
+  // TODO: fallback
+  return <Suspense fallback={<p>Loading</p>}>{renderBasingOnSettings()}</Suspense>;
+}
