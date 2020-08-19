f898f13543686b003680d293df466ccf7d60ad93
Artem Piskarev <artyeug@gmail.com>
2020-08-20 01:32:51 +0700

    Implement IndexPage

---
+++ src/pages/indexPage/IndexPage.tsx
@@ -0,0 +1,12 @@
+import "./IndexPage.scss";
+import React from "react";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <p>Test</p>
+      <p>Pest</p>
+      <p>Fest</p>
+    </div>
+  );
+}
