9cf4dfab8da16322e7ee4a18048806dda8c018ab
Artem Piskarev <artyeug@gmail.com>
2020-08-23 20:59:00 +0700

    Update IndexPage (add fetching records functionality)

--- src/pages/indexPage/IndexPage.tsx
+++ src/pages/indexPage/IndexPage.tsx
@@ -1,19 +1,53 @@
 import { css } from "@emotion/core";
 import React from "react";
-import { Col, Container, Row, Table } from "reactstrap";
+import { Button, Card, CardBody, Col, Container, Row, Table, Collapse, Spinner } from "reactstrap";
+import AddRecordForm from "../../components/forms/addRecordForm";
+import RecordsActions from "../../store/actions/recordsActions";
+import { useDispatch, useSelector } from "react-redux";
+import RecordsSelectors from "../../store/selectors/recordsSelectors";
 
 export default function IndexPage() {
+  const dispatch = useDispatch();
+  const records = useSelector(RecordsSelectors.records);
+
+  const [isAddRecordFormCollapseOpen, setIsAddRecordFormCollapseOpen] = React.useState(false);
+
+  const getRecords = React.useCallback(() => {
+    dispatch(RecordsActions.fetchRecords());
+  }, []);
+
+  React.useEffect(() => {
+    getRecords();
+  }, [getRecords]);
+
   return (
     <Container
-      css={css({
-        margin: "32px 0",
-      })}
-      fluid
+      css={css`
+        margin: 32px 0;
+      `}
+      fluid="md"
     >
+      <Row className="justify-content-start">
+        <Col xl={6} lg={6}>
+          <Button
+            color="primary"
+            onClick={() => setIsAddRecordFormCollapseOpen((prevState) => !prevState)}
+            style={{ marginBottom: "1rem" }}
+          >
+            Add new record
+          </Button>
+          <Collapse isOpen={isAddRecordFormCollapseOpen}>
+            <Card>
+              <CardBody>
+                <AddRecordForm />
+              </CardBody>
+            </Card>
+          </Collapse>
+        </Col>
+      </Row>
       <Row className="justify-content-center">
-        <Col xl={10}>
-          <h1>Test H1</h1>
-
+        <Col>
+          <h1>Your training records</h1>
           <Table responsive hover>
             <thead>
               <tr>
@@ -24,56 +58,27 @@ export default function IndexPage() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>{new Date().toLocaleDateString("en-US")}</td>
-                <td>Sprint</td>
-                <td>3.0</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>{new Date(2020, 5, 20).toLocaleDateString("en-US")}</td>
-                <td>Swimming</td>
-                <td>1.15</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>{new Date().toLocaleDateString("en-US")}</td>
-                <td>Sprint</td>
-                <td>3.0</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>{new Date(2020, 5, 20).toLocaleDateString("en-US")}</td>
-                <td>Swimming</td>
-                <td>1.15</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>{new Date().toLocaleDateString("en-US")}</td>
-                <td>Sprint</td>
-                <td>3.0</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>{new Date(2020, 5, 20).toLocaleDateString("en-US")}</td>
-                <td>Swimming</td>
-                <td>1.15</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>{new Date().toLocaleDateString("en-US")}</td>
-                <td>Sprint</td>
-                <td>3.0</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>{new Date(2020, 5, 20).toLocaleDateString("en-US")}</td>
-                <td>Swimming</td>
-                <td>1.15</td>
-              </tr>
+              {records.data.map((record, index) => (
+                <tr key={record.id}>
+                  <td>{index + 1}</td>
+                  <td>{new Date(record.date).toLocaleDateString("en-US")}</td>
+                  <td>{record.type}</td>
+                  <td>{record.mileage.toFixed(2)}</td>
+                </tr>
+              ))}
             </tbody>
           </Table>
+          {(records.isLoading || records.isError) && (
+            <div className="d-flex flex-column align-items-center">
+              {records.isLoading && <Spinner className="" />}
+              {records.isError && (
+                <React.Fragment>
+                  <p>Error while fetching the data</p>
+                  <Button onClick={getRecords}>Retry</Button>
+                </React.Fragment>
+              )}
+            </div>
+          )}
         </Col>
       </Row>
     </Container>
