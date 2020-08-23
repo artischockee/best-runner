22e6ade5561d7b56868131cea0feec44825e58ae
Artem Piskarev <artyeug@gmail.com>
2020-08-23 20:53:57 +0700

    Configure redux store; inject it in Root component

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Provider } from 'react-redux';
 import { Router } from "react-router-dom";
+import { Redux } from "../store/types";
 import history from "../services/router/history";
 import Routes from "../services/router/config";
 import App from "./app";
 
-export default function Root() {
+interface Props {
+  store: Redux.ApplicationStore;
+}
+
+export default function Root(props: Props) {
   return (
-    <Router history={history}>
-      <App routes={Routes} />
-    </Router>
+    <Provider store={props.store}>
+      <Router history={history}>
+        <App routes={Routes} />
+      </Router>
+    </Provider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import "./index.scss";
 import React from "react";
 import { render } from "react-dom";
+import { configureStore } from "@reduxjs/toolkit";
 import Root from "./components/Root";
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 render(
   <React.StrictMode>
-    <Root />
+    <Root store={store} />
   </React.StrictMode>,
   document.getElementById("root")
 );
