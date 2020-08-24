d67c1d263c10aff90236ce3a8a523b1eabfd9424
Artem Piskarev <artyeug@gmail.com>
2020-08-25 01:16:17 +0700

    Implement RecordRowHooks

---
+++ src/components/recordRow/RecordRowHooks.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import TrainingTypesSelectors from "../../store/selectors/trainingTypesSelectors";
+import { TrainingTypesReducerState } from "../../store/reducers/trainingTypesReducer";
+import DictionariesUtils from "../../utils/dictionariesUtils";
+
+export default class RecordRowHooks {
+  static useTrainingTypes(): [
+    TrainingTypesReducerState,
+    ((id: number) => string | null)
+  ] {
+    const trainingTypes = useSelector(TrainingTypesSelectors.trainingTypes);
+
+    const getTypeLabelById = React.useCallback((id: number) => {
+      const typeEntry = trainingTypes.data.find((tt) => tt.id === id);
+      if (typeEntry == null) return null;
+
+      return DictionariesUtils.getTrainingTypeL10nKey(typeEntry.value);
+    }, []);
+
+    return [trainingTypes, getTypeLabelById];
+  }
+}
