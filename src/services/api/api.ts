acf09bc2b1bf4f846708df7a07764757f92b1372
Artem Piskarev <artyeug@gmail.com>
2020-08-24 14:42:28 +0700

    Add postRecord(), getTrainingTypes() APIs

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,7 +1,16 @@
+import { Data } from "../../types/data";
 import http from "./apiConfig";
 
 export default class Api {
   static getRecords() {
     return http.get("records");
+  }
+
+  static postRecord(data: Data.RecordPost) {
+    return http.post("records", data);
+  }
+
+  static getTrainingTypes() {
+    return http.get("dictionaries/training-types");
   }
 }
