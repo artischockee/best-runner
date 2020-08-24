9df25a27aa34353834313b81997ccf2830078438
Artem Piskarev <artyeug@gmail.com>
2020-08-25 01:11:33 +0700

    Remove output.path from common webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,9 @@
-const path = require("path");
 const WebpackManifestPlugin = require("webpack-manifest-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "build"),
-  },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
   },
