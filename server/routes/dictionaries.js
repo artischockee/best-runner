9fe102f5beba72f2d1cb49ad1ec147a13de9fbab
Artem Piskarev <artyeug@gmail.com>
2020-08-24 14:40:25 +0700

    Update dictionaries.js and records.js

--- server/routes/dictionaries.js
+++ server/routes/dictionaries.js
@@ -2,14 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-const __trainingTypes = {
-  running: "running",
-  cycling: "cycling",
-  skiing: "skiing",
-  walking: "walking",
-};
+const __trainingTypes = [
+  {
+    id: 0,
+    value: "running",
+  },
+  {
+    id: 1,
+    value: "cycling",
+  },
+  {
+    id: 2,
+    value: "skiing",
+  },
+  {
+    id: 3,
+    value: "walking",
+  },
+];
 
-router.get("/dictionaries", (req, res) => {
+router.get("/training-types", (req, res) => {
   res.json(__trainingTypes);
 });
 
--- server/routes/records.js
+++ server/routes/records.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { getReducedErrors, someObjectArrayNotEmpty } = require("../utils/objectUtils");
+const { getRecordPostErrors } = require("../utils/recordsUtils");
 const __trainingTypes = require("./dictionaries").trainingTypes;
 
 const router = express.Router();
@@ -6,26 +8,29 @@ const router = express.Router();
 const __records = [
   {
     id: 0,
-    date: new Date(2020, 0, 1).toISOString(),
-    type: __trainingTypes.cycling,
+    date: new Date("2020-01-01").toISOString(),
+    type: __trainingTypes[0].id,
     mileage: 15.1,
+    comments: "",
   },
   {
     id: 1,
-    date: new Date(2020, 0, 2).toISOString(),
-    type: __trainingTypes.running,
+    date: new Date("2020-02-02").toISOString(),
+    type: __trainingTypes[1].id,
     mileage: 10.25,
+    comments: "",
   },
   {
     id: 2,
-    date: new Date(2020, 0, 3).toISOString(),
-    type: __trainingTypes.skiing,
+    date: new Date("2020-03-03").toISOString(),
+    type: __trainingTypes[2].id,
     mileage: 20.25,
+    comments: "",
   },
   {
     id: 3,
-    date: new Date(2020, 0, 4).toISOString(),
-    type: __trainingTypes.walking,
+    date: new Date("2020-04-04").toISOString(),
+    type: __trainingTypes[3].id,
     mileage: 5,
   },
 ];
@@ -34,8 +39,24 @@ router.get("/", (req, res) => {
   res.json(__records);
 });
 
-router.post("/", (req) => {
-  console.log("\n\nPOST\n\n", req);
+router.post("/", (req, res) => {
+  const errors = getRecordPostErrors(req.body);
+
+  if (someObjectArrayNotEmpty(errors)) {
+    return res.status(400).json({ errors: getReducedErrors(errors) });
+  }
+
+  const newRecord = {
+    id: __records[__records.length - 1].id + 1,
+    date: new Date(req.body.date).toISOString(),
+    type: __trainingTypes.find((tt) => tt.value === req.body.type).id,
+    mileage: Number(req.body.mileage.replace(/^(\d+),?(\d*)$/i, "$1.$2")),
+    comments: req.body.comments,
+  };
+
+  __records.push(newRecord);
+
+  res.status(201).send(newRecord);
 });
 
 // router.put("/", (req, res) => {
