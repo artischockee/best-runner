d8cf0079361ae5e4edc4a8540b05f76614e3513c
Artem Piskarev <artyeug@gmail.com>
2020-08-25 01:13:21 +0700

    Add new color constants

--- src/constants/styleConstants.ts
+++ src/constants/styleConstants.ts
@@ -6,6 +6,8 @@ const StyleConstants = {
     accent: "#ffa07a",
     accentHover: "#f78c61",
     accentShadow: "rgba(247, 140, 97, .5)",
+    danger: "#dc3545",
+    textDefault: "#212529",
     textW500: "#323232",
   },
 };
