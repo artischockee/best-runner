0780a7fb66e59ef8e856994fd3c2a9cc66e8dd87
Artem Piskarev <artyeug@gmail.com>
2020-08-25 01:15:00 +0700

    Fix 'getTrainingTypeL10nKey' method's return value

--- src/utils/dictionariesUtils.ts
+++ src/utils/dictionariesUtils.ts
@@ -1,8 +1,6 @@
 export default class DictionariesUtils {
   static getTrainingTypeL10nKey(value: string) {
-    return value;
-
     // FIXME: can't use localiser.l() because of issues
-    // return `dictionaries/trainingTypes/${value}`;
+    return `dictionaries/trainingTypes/${value}`;
   }
 }
