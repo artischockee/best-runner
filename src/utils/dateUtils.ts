b9bf1716253df0bbb1ab5f796f18d3422446d2d1
Artem Piskarev <artyeug@gmail.com>
2020-08-25 01:14:34 +0700

    Add 'getISODate' method

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -5,4 +5,10 @@ export default class DateUtils {
   static getTodayDateISO(): string {
     return new Date().toISOString().split("T")[0];
   }
+
+  static getISODate(fullISOString: string): string | null {
+    if (!/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(fullISOString)) return null;
+
+    return fullISOString.split("T")[0];
+  }
 }
