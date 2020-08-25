801efed03d420a93571008923b8a05c49cfb8214
Artem Piskarev <artyeug@gmail.com>
2020-08-25 23:09:32 +0700

    Add static files serving

--- server/app.js
+++ server/app.js
@@ -1,16 +1,21 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const recordsRouter = require("./routes/records");
 const dictionariesRouter = require("./routes/dictionaries").router;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use(express.static(__dirname + "/"));
 app.use("/api/records", recordsRouter);
 app.use("/api/dictionaries", dictionariesRouter);
 
-app.listen(port, () => {
-  console.log(`BestRunner app server at http://localhost:${port}`);
-});
+app.listen(port);
