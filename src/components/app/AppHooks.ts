916846499599a79392338de4c12bf3c5889571b6
Artem Piskarev <artyeug@gmail.com>
2020-08-25 20:41:06 +0700

    Remove "version" from 'useConsoleReport'

--- src/components/app/AppHooks.ts
+++ src/components/app/AppHooks.ts
@@ -5,7 +5,6 @@ export default class AppHooks {
     React.useEffect(() => {
       console.group("BestRunner");
       console.log("Demo application");
-      console.log(`Version ${process.env.REACT_APP_VERSION}`);
       console.log('Artem "artischocke" Piskarev');
       console.groupEnd();
     }, []);
