deac488cf1c286c97bb81a0533b0c4fb94b317eb
Artem Piskarev <artyeug@gmail.com>
2020-08-26 03:01:46 +0700

    Remove unused method

--- src/types/utilTypes.d.ts
+++ src/types/utilTypes.d.ts
@@ -1,8 +1,3 @@
-import React from "react";
-
 export declare namespace UtilTypes {
   type SearchParams = Record<string, string | string[]>;
-
-  // TODO: remove to some new types file (e.g. reactHooks.d.ts)
-  type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
 }
