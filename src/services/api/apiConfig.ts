1cfac02820df425ec70bf028fc68bc7406cda454
Artem Piskarev <artyeug@gmail.com>
2020-08-25 23:55:37 +0700

    Fix prod stand URL

--- src/services/api/apiConfig.ts
+++ src/services/api/apiConfig.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const stands = {
   dev: "http://192.168.0.101:5000/",
-  // dev: 'http://localhost:5000/',
-  prod: "http://127.0.0.1:8000/",
+  prod: "/",
 };
 
 export const StandUrl = process.env.NODE_ENV !== "production" ? stands.dev : stands.prod;
